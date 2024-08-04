51c34a81f1bded60eee5b6a1feb8990afa0b2708
Thakur Neupane <thakur.neupane.neupane@gmail.com>
2024-08-04 14:38:19 +1000

    sign up ui

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8001",
+        changeOrigin: true, // Add this line if you have issues with CORS
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
